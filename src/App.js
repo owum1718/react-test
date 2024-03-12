7b2ac2eb967d370f5bb40d6a79fc2b74d27ff286
anjum <ow.um1718@gmail.com>
2024-03-12 22:29:13 +0530

    saved initially

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { TextField } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">Amistee</h1>
+
+      <form>
+        <div>
+          <select>
+            <option value="Service Needed">Service Needed </option>
+            <option value="Service Needed">
+              Residential Air Duct Cleanaing{" "}
+            </option>
+            <option value="Service Needed">
+              Commercial Air Duct Cleanaing
+            </option>
+            <option value="Service Needed">Insulation </option>
+            <option value="Service Needed">
+              Duct Sealing Powered By Aeroseal&reg;
+            </option>
+            <option value="Service Needed">
+              Duct Lining Powered By Duct Armor&reg;{" "}
+            </option>
+          </select>
+        </div>
+        <div>
+          <TextField
+            placeholder="First Name"
+            id="outlined-basic"
+            variant="outlined"
+            size="small"
+          />
+
+          <TextField
+            placeholder="Last Name"
+            id="outlined-basic"
+            variant="outlined"
+            size="small"
+          />
+        </div>
+        <div>
+          <TextField placeholder="Zip*" size="small"></TextField>
+          <TextField placeholder="city*" size="small"></TextField>
+          <TextField placeholder="State*" size="small"></TextField>
+        </div>
+        <div>
+          <TextField cols="30" rows="10" placeholder="Address*"></TextField>
+        </div>
+        <div>
+          <TextField placeholder="Phone*" size="small"></TextField>
+          <TextField placeholder="email*" size="small"></TextField>
+        </div>
+        <div>
+          <TextField placeholder="How did you first hear about Amistee?*"></TextField>
+        </div>
+
+        <div>
+          <select>
+            <option value="" key="">
+              Are you re-scheduling an existing appointment?{" "}
+            </option>
+            <option>Yes</option>
+            <option>No</option>
+          </select>
+        </div>
+        <div>
+          <select>
+            <option value='' key=''> 
+            placeholder="Approx.Sq. Footage of Living Space(Not Including Basement"</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 }
